c89cd622f59e0961742e9a65f5548c725a6b0c12
kishore <4nm15is025@nmamit.in>
2018-11-05 15:50:03 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/status',(req,res)=>{
+  res.send({
+    message:'hello world!'
+  })
+})
+
+app.listen(process.env.PORT || 8081)
