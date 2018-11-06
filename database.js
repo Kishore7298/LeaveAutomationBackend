b7e4c1443055f0ba1972a8620900477cffb8f3f7
kishore <4nm15is025@nmamit.in>
2018-11-06 12:52:51 +0530

    adding student side api's for authorizing, pending list, approved list

---
+++ database.js
@@ -0,0 +1,34 @@
+db.letters.insert([
+    {
+        email:"kishoreas7298@gmail.com",
+        password:"1234",
+        name:"Kishore",
+        to:"Karthik",
+        reference:"vasudev",
+        body:"i want leave on febrauary 16th of this month",
+        referenceApproved:false,
+        toArrproved:false
+    },
+    {
+        email:"nandan7298@gmail.com",
+        password:"1234",
+        name:"Nandan",
+        to:"Karthik",
+        reference:"vasudev",
+        body:"i want leave on febrauary 16th of this month",
+        referenceApproved:false,
+        toArrproved:false
+    },
+    {
+        email:"aditya7298@gmail.com",
+        password:"1234",
+        name:"Aditya",
+        to:"Karthik",
+        reference:"vasudev",
+        body:"i want leave on febrauary 16th of this month",
+        referenceApproved:false,
+        toArrproved:false
+    },
+    
+    
+])
--- src/app.js
+++ src/app.js
@@ -2,14 +2,66 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/status',(req,res)=>{
-  res.send({
-    message:'hello world!'
-  })
+app.get('/auth/:email-:password',(req,res)=>{
+    MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("leaveautomation");
+    dbo.collection("letters").find({email:req.params.email, password:req.params.password},{ projection: { _id: 0, email: 1,} }).toArray(function(err, result) {
+      if (err) throw err;
+      res.send({
+        email: result,
+        length: result.length
+      });
+      db.close();
+    });
+  }); 
+})
+
+app.get('/pending/:email',(req,res)=>{
+  MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("leaveautomation");
+  dbo.collection("letters").find({email:req.params.email },{ projection: { _id: 0, to:1, reference:1, body:1} }).toArray(function(err, result) {
+    if (err) throw err;
+    res.send(result);
+    db.close();
+  });
+}); 
+})
+
+app.get('/approved/:email',(req,res)=>{
+  MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("leaveautomation");
+  dbo.collection("letters").find({email:req.params.email, toArrproved:true },{ projection: { _id: 0, to:1, reference:1, body:1} }).toArray(function(err, result) {
+    if (err) throw err;
+    res.send(result);
+    db.close();
+  });
+}); 
+})
+
+app.get('/status/:id',(req,res)=>{
+  MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("leaveautomation");
+  dbo.collection("letters").find({_id:req.params.id}).toArray(function(err, result) {
+    if (err) throw err;
+    res.send(result);
+    db.close();
+  });
+}); 
+})
+
+app.get('/a/:email-:password',(req,res)=>{
+  res.send(req.params);
 })
 
 app.listen(process.env.PORT || 8081)
