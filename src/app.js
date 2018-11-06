318a22f71134282c648a668d55f8516e221a77da
kishore <4nm15is025@nmamit.in>
2018-11-06 21:18:18 +0530

    Tuning api's

--- src/app.js
+++ src/app.js
@@ -1,51 +1,53 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const morgan = require('morgan');
 
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
 const app = express();
+app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/auth/:email-:password',(req,res)=>{
+app.get('/auth',(req,res)=>{
     MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
+    if (err) console.log(err);
     var dbo = db.db("leaveautomation");
-    dbo.collection("letters").find({email:req.params.email, password:req.params.password},{ projection: { _id: 0, email: 1,} }).toArray(function(err, result) {
-      if (err) throw err;
+    dbo.collection("letters").find({email:req.query.email, password:req.query.password},{ projection: { _id: 0, email: 1,} }).toArray(function(err, result) {
+      if (err) console.log(err);
       res.send({
         email: result,
         length: result.length
       });
       db.close();
     });
-  }); 
+  });
 })
 
-app.get('/pending/:email',(req,res)=>{
+app.get('/pending/',(req,res)=>{
   MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("leaveautomation");
-  dbo.collection("letters").find({email:req.params.email },{ projection: { _id: 0, to:1, reference:1, body:1} }).toArray(function(err, result) {
+  dbo.collection("letters").find({email:req.query.email },{ projection: { _id: 0, to:1, reference:1, body:1} }).toArray(function(err, result) {
     if (err) throw err;
     res.send(result);
     db.close();
   });
-}); 
+});
 })
 
-app.get('/approved/:email',(req,res)=>{
+app.get('/approved/',(req,res)=>{
   MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("leaveautomation");
-  dbo.collection("letters").find({email:req.params.email, toArrproved:true },{ projection: { _id: 0, to:1, reference:1, body:1} }).toArray(function(err, result) {
+  dbo.collection("letters").find({email:req.query.email, toArrproved:true },{ projection: { _id: 0, to:1, reference:1, body:1} }).toArray(function(err, result) {
     if (err) throw err;
     res.send(result);
     db.close();
   });
-}); 
+});
 })
 
 app.get('/status/:id',(req,res)=>{
@@ -57,11 +59,8 @@ app.get('/status/:id',(req,res)=>{
     res.send(result);
     db.close();
   });
-}); 
-})
+});
+});
 
-app.get('/a/:email-:password',(req,res)=>{
-  res.send(req.params);
-})
 
-app.listen(process.env.PORT || 8081)
+app.listen(process.env.PORT || 8082)
