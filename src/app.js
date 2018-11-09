48e063a5194c9c0a8af44b8b5ed925d390b15c0a
kishore <4nm15is025@nmamit.in>
2018-11-09 16:42:40 +0530

    updating status

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const morgan = require('morgan');
 
 var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
 var url = "mongodb://localhost:27017/";
 
 const app = express();
@@ -11,6 +12,8 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
+//user authentication with email and password.
+//returns email and name
 app.get('/auth',(req,res)=>{
     MongoClient.connect(url, function(err, db) {
     if (err) console.log(err);
@@ -19,6 +22,23 @@ app.get('/auth',(req,res)=>{
       if (err) console.log(err);
       res.send({
         email: result,
+        length: result.length
+      });
+      db.close();
+    });
+  });
+});
+
+//admin or lecturer authentication with email and password.
+//returns just name
+app.get('/adminauth',(req,res)=>{
+    MongoClient.connect(url, function(err, db) {
+    if (err) console.log(err);
+    var dbo = db.db("leaveautomation");
+    dbo.collection("adminLogin").find({"email":req.query.email, "password":req.query.password},{ projection: { _id: 0, name:1} }).toArray(function(err, result) {
+      if (err) console.log(err);
+      res.send({
+        name: result,
         length: result.length
       });
       db.close();
@@ -36,13 +56,53 @@ app.get('/pending',(req,res)=>{
     db.close();
   });
 });
-})
+});
+
+//admin route to get the requests
+app.get('/request',(req,res)=>{
+  MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("leaveautomation");
+  dbo.collection("letters").find({to:req.query.name, toApproved:false, throughApproved:true},{ projection: { _id: 1, name:1, through:1,sub:1, body:1} }).toArray(function(err, result) {
+    if (err) throw err;
+    res.send(result);
+    db.close();
+  });
+});
+});
+
+//admin route to get reference
+app.get('/reference',(req,res)=>{
+  MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("leaveautomation");
+  dbo.collection("letters").find({through:req.query.name, throughApproved:false},{ projection: { _id: 1, name:1, through:1,sub:1, body:1} }).toArray(function(err, result) {
+    if (err) throw err;
+    res.send(result);
+    db.close();
+  });
+});
+});
+
+//admin route to get the approved list
+app.get('/adminApproved',(req,res)=>{
+  MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("leaveautomation");
+  dbo.collection("letters").find({$or:[{through:req.query.name, throughApproved:true},{to:req.query.name, toApproved:true}]},{ projection: { _id: 1, name:1, through:1,sub:1, body:1} })
+  .toArray(function(err, result) {
+    if (err) throw err;
+    res.send(result);
+    db.close();
+  });
+});
+});
 
 app.get('/approved',(req,res)=>{
   MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("leaveautomation");
-  dbo.collection("letters").find({email:req.query.email, toArrproved:true },{ projection: { _id: 0, to:1, reference:1, body:1} }).toArray(function(err, result) {
+  dbo.collection("letters").find({email:req.query.email, toApproved:true },{ projection: { _id: 1, to:1, through:1,sub:1, body:1} }).toArray(function(err, result) {
     if (err) throw err;
     res.send(result);
     db.close();
@@ -50,11 +110,12 @@ app.get('/approved',(req,res)=>{
 });
 })
 
-app.get('/status/:id',(req,res)=>{
+app.get('/status',(req,res)=>{
+  id = req.query.id
   MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("leaveautomation");
-  dbo.collection("letters").find({_id:req.params.id}).toArray(function(err, result) {
+  dbo.collection("letters").find({_id: new ObjectID(id)}).toArray(function(err, result) {
     if (err) throw err;
     res.send(result);
     db.close();
@@ -75,10 +136,29 @@ app.get('/admins',(req,res)=>{
 });
 
 app.post('/submit',(req,res)=>{
-    var email = req.body.to;
-
-    res.send({status:email});
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("leaveautomation");
+    dbo.collection("letters").insertOne(req.body , function(err, res) {
+      if (err) throw err;
+      db.close();
+    });
+  });
 });
 
+app.post('/approve',(req,res)=>{
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("leaveautomation");
+    dbo.collection("letters").update({_id:`ObjectId("${req.body.id}")`} ,{ $set:
+      {
+        "toApproved" : true
+      }
+   }, function(err, res) {
+      if (err) throw err;
+      db.close();
+    });
+  });
+});
 
 app.listen(process.env.PORT || 8082)
